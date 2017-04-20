b689ded298b8ebc0a034b831d0977188e3e771af
orian_galkowicz <orian.galkowicz@ladbrokescoarl.com>
2017-04-20 09:58:26 +0300

    - moved styling to external file - added firebase registration

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,16 +3,9 @@
     <img src="../assets/logo.png">
     <h1>{{ msg }}</h1>
     <div class="options container">
-      <ul>
-        <li>
-          <div class="login btn-large btn-block section">
-            <router-view to="/register"></router-view>
-            LOGIN
-          </div>
-        </li>
-        <div class="new-user btn-large btn-block section">NEW USER</div>
-        <div class="on-the-fly btn-large btn-block section">ON THE FLY</div>
-      </ul>
+      <router-link to="/"><div class="login btn-large btn-block section">LOGIN</div></router-link>
+      <router-link to="/register"><div class="new-user btn-large btn-block section">NEW USER</div></router-link>
+      <router-link to="/"><div class="on-the-fly btn-large btn-block section">ON THE FLY</div> </router-link>
     </div>
   </div>
 </template>
@@ -22,7 +15,7 @@ export default {
   name: 'hello',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Lo Ko Hot - Fair Teams!',
     };
   },
 };
@@ -50,9 +43,10 @@ a {
   .container {
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
   }
   .btn-large {
     flex-basis: content;
+    font-size: 3rem;
   }
 </style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,29 +1,66 @@
 <template>
-  <div class="login-form">
+  <div class="login-form center-align" >
     <div class="options container">
-      <img src="../assets/logo.png">
-      <img src="../assets/logo.png">
-      <img src="../assets/logo.png">
+        <div class="username">
+          <input v-model="email" id="email" type="text" placeholder="Email...">
+        </div>
+        <div class="password">
+          <input v-model="password" id="password" type="password" placeholder="Password...">
+        </div>
+        <div class="center-align">
+          <button v-on:click="register" id="sign-in">Register</button>
+          <button v-on:click="signInWithGoogle" id="sign-in-g">Sign-in with Google</button>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
 
-//  const firebase = require("firebase/app");
-//  const auth = firebase.auth();
-//  auth.signInWithEmailAndPassword(email, pass);
+  const firebase = require('firebase/app');
+
   export default {
     name: 'hello',
     data() {
       return {
         msg: 'Welcome to Your Vue.js App',
+        email: '',
+        password: '',
       };
+    },
+    methods: {
+      register() {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+          .catch((error) => {
+            console.log('register error', error);
+            if (error.code === 'auth/email-already-in-use') {
+              const credntial = firebase.auth.EmailAuthProvider.credential(this.email, this.password);
+              this.signInWithGoogle()
+                .then(() => {
+                  firebase.auth().currentUser.link(credntial)
+                    .then((user) => {
+                      console.log('Account linking success', user);
+                    }, (googleError) => {
+                      console.log('Account linking error', googleError);
+                    });
+                });
+            }
+          });
+        console.debug(this.email);
+      },
+      signInWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('profile');
+        provider.addScope('email');
+
+        return firebase.auth().signInWithPopup(provider)
+          .catch((error) => {
+            console.log('Google sign in error', error);
+          });
+      },
     },
   };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style lang="scss" scoped src="./register.scss"></style>
